7c56f8a2c0c8b5b0a532f15665e330c22454d9e1
Shreel Patel <patelshreel001@gmail.com>
2024-02-07 15:59:01 -0500

    Typo fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
-const getDate = require('./getDate')
+const getDate = require('./utils/getDate')
 
 const app = express();
 app.use(express.static('public'));
